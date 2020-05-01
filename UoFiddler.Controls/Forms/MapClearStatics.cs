0bd42a729153cf0702c90b36a992c08451abeb3c
AsY!um- <web@asylum.pl>
2020-05-01 21:09:36 +0200

    Remove unused using.

--- UoFiddler.Controls/Forms/MapClearStatics.cs
+++ UoFiddler.Controls/Forms/MapClearStatics.cs
@@ -1,9 +1,9 @@
 ﻿/***************************************************************************
  *
  * $Author: Turley
- * 
+ *
  * "THE BEER-WARE LICENSE"
- * As long as you retain this notice you can do whatever you want with 
+ * As long as you retain this notice you can do whatever you want with
  * this stuff. If we meet some day, and you think this stuff is worth it,
  * you can buy me a beer in return.
  *
@@ -11,7 +11,6 @@
 
 using System;
 using System.Windows.Forms;
-using Ultima;
 using UoFiddler.Controls.Classes;
 using Map = UoFiddler.Controls.UserControls.Map;
 
