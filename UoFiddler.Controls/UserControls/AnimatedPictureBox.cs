8f09fdd6d59645318077ae7cad4cca0a68540bd1
AsY!um- <377468+AsYlum-@users.noreply.github.com>
2025-02-09 12:30:03 +0100

    Cleanup - AnimatedPictureBox.

--- UoFiddler.Controls/UserControls/AnimatedPictureBox.cs
+++ UoFiddler.Controls/UserControls/AnimatedPictureBox.cs
@@ -13,24 +13,27 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using UoFiddler.Controls.Classes;
 
 namespace UoFiddler.Controls.UserControls
 {
     [Designer(typeof(System.Windows.Forms.Design.ControlDesigner))]
-    public partial class AnimatedPictureBox : PictureBox
+    public class AnimatedPictureBox : PictureBox
     {
         private List<AnimatedFrame> _frames;
         private int _frameIndex;
-        private Timer _timer;
+        private readonly Timer _timer;
         private bool _animate;
         private bool _showFrameBounds;
         private Size _animationSize;
         private Point _drawCenter;
         private Point _draggedOffset = new(0, 0);
         private Point _mouseDownLocation;
-        private bool _isDragging = false;
+        private bool _isDragging;
+
+        public event EventHandler FrameChanged;
 
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public List<AnimatedFrame> Frames
@@ -59,23 +62,25 @@ namespace UoFiddler.Controls.UserControls
             get => _frames?.FirstOrDefault();
         }
 
-        public event EventHandler FrameChanged;
         public AnimatedFrame CurrentFrame
         {
             get => _frames?[_frameIndex];
         }
+
         public int FrameIndex
         {
             get => _frameIndex;
             set
             {
                 var newValue = value % Math.Max(_frames?.Count ?? 1, 1);
-                if (_frameIndex != newValue)
+                if (_frameIndex == newValue)
                 {
-                    _frameIndex = newValue;
-                    FrameChanged?.Invoke(this, EventArgs.Empty);
-                    Invalidate();
+                    return;
                 }
+
+                _frameIndex = newValue;
+                FrameChanged?.Invoke(this, EventArgs.Empty);
+                Invalidate();
             }
         }
 
@@ -150,64 +155,76 @@ namespace UoFiddler.Controls.UserControls
 
         private void OnMouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            if (e.Button != MouseButtons.Left)
             {
-                _mouseDownLocation = e.Location;
-                _isDragging = true;
-                Cursor = Cursors.SizeAll; // Change cursor to dragging cursor
+                return;
             }
+
+            _mouseDownLocation = e.Location;
+            _isDragging = true;
+            Cursor = Cursors.SizeAll; // Change cursor to dragging cursor
         }
 
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            if (_isDragging)
+            if (!_isDragging)
             {
-                // Calculate the new center point based on the mouse movement
-                _draggedOffset.X += e.X - _mouseDownLocation.X;
-                _draggedOffset.Y += e.Y - _mouseDownLocation.Y;
-                // Update the mouse down location to the new location
-                _mouseDownLocation = e.Location;
-                // Refresh the PictureBox to trigger the Paint event
-                Invalidate();
+                return;
             }
+
+            // Calculate the new center point based on the mouse movement
+            _draggedOffset.X += e.X - _mouseDownLocation.X;
+            _draggedOffset.Y += e.Y - _mouseDownLocation.Y;
+
+            // Update the mouse down location to the new location
+            _mouseDownLocation = e.Location;
+
+            // Refresh the PictureBox to trigger the Paint event
+            Invalidate();
         }
 
         private void OnMouseUp(object sender, MouseEventArgs e)
         {
-            if (_isDragging)
+            if (!_isDragging)
             {
-                _isDragging = false;
-                Cursor = Cursors.Default; // Change cursor back to default
+                return;
             }
+
+            _isDragging = false;
+            Cursor = Cursors.Default; // Change cursor back to default
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (_frames.Count > 0)
+            if (_frames.Count == 0)
             {
-                _frameIndex = (_frameIndex + 1) % _frames.Count;
-                FrameChanged?.Invoke(this, EventArgs.Empty);
-                Invalidate(); // Force a repaint
+                return;
             }
+
+            _frameIndex = (_frameIndex + 1) % _frames.Count;
+            FrameChanged?.Invoke(this, EventArgs.Empty);
+            Invalidate(); // Force a repaint
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
             AnimatedFrame frame = _frameIndex < _frames?.Count ? _frames[_frameIndex] : null;
-            if (frame != null)
+            if (frame == null)
             {
-                var location = new Point(
-                    _drawCenter.X - frame.Center.X + (Width - _animationSize.Width) / 2 + _draggedOffset.X,
-                    _drawCenter.Y - frame.Center.Y - frame.Bitmap.Height + (Height - _animationSize.Height) / 2 + _draggedOffset.Y
-                );
+                return;
+            }
 
-                e.Graphics.DrawImage(frame.Bitmap, location);
+            var location = new Point(
+                _drawCenter.X - frame.Center.X + (Width - _animationSize.Width) / 2 + _draggedOffset.X,
+                _drawCenter.Y - frame.Center.Y - frame.Bitmap.Height + (Height - _animationSize.Height) / 2 + _draggedOffset.Y
+            );
 
-                if (_showFrameBounds)
-                {
-                    e.Graphics.DrawRectangle(new Pen(Color.Red), new Rectangle(location, frame.Bitmap.Size));
-                }
+            e.Graphics.DrawImage(frame.Bitmap, location);
+
+            if (_showFrameBounds)
+            {
+                e.Graphics.DrawRectangle(new Pen(Color.Red), new Rectangle(location, frame.Bitmap.Size));
             }
         }
 
@@ -216,6 +233,7 @@ namespace UoFiddler.Controls.UserControls
             if (disposing)
             {
                 _timer.Dispose();
+
                 if (_frames != null)
                 {
                     foreach (var frame in _frames)
@@ -224,12 +242,14 @@ namespace UoFiddler.Controls.UserControls
                     }
                 }
             }
+
             base.Dispose(disposing);
         }
 
         public void Reset()
         {
             _frameIndex = 0;
+
             if (_frames != null)
             {
                 foreach (var frame in _frames)
@@ -237,10 +257,12 @@ namespace UoFiddler.Controls.UserControls
                     frame.Bitmap?.Dispose();
                 }
             }
+
             _frames = [];
             _timer.Stop();
             _animate = false;
             _showFrameBounds = false;
+
             Invalidate();
         }
     }
